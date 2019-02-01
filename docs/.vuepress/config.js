e3ddc65affb205d69c6a2def1758763f16c62aaa
vittominacori <vittoriominacori@gmail.com>
2019-02-01 10:56:55 +0100

    feat(audit): add docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: 'Skillchain Audit done by BC1 - Blockchain Pioneers',
+  description: 'Skillchain Audit done by BC1 - Blockchain Pioneers',
+  base: '/skillchain-audit/',
+  head: [
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:url', content: 'https://bc1tech.github.io/skillchain-audit/' }],
+    ['meta', { property: 'og:image', content: 'https://bc1tech.github.io/skillchain-audit/assets/images/og-image.png' }],
+    ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { property: 'twitter:image', content: 'https://bc1tech.github.io/skillchain-audit/assets/images/og-image.png' }],
+    ['meta', { property: 'twitter:title', content: 'Skillchain Audit done by BC1 - Blockchain Pioneers' }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+  ],
+  themeConfig: {
+    repo: 'bc1tech/skillchain-audit',
+    sidebar: 'auto',
+  },
+};
